287c99eab652915155c400411a262b6ba9355e89
Djamel F <djamelfel@gmail.com>
2016-01-04 04:17:14 +0100

    Ajout de l'activité login

---
+++ app/src/main/java/djamelfel/communitycargps/LoginActivity.java
@@ -0,0 +1,237 @@
+package djamelfel.communitycargps;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+/**
+ * A login screen that offers login via login/password.
+ */
+public class LoginActivity extends ActionBarActivity { //implements LoaderCallbacks<Cursor> {
+
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+    private UserLoginPostgresql mAuthTask = null;
+
+    // UI references.
+    private AutoCompleteTextView mLoginView;
+    private EditText mPasswordView;
+    private View mProgressView;
+    private View mLoginFormView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // Set up the login form.
+        mLoginView = (AutoCompleteTextView) findViewById(R.id.login);
+
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button mLoginSignInButton = (Button) findViewById(R.id.login_sign_in_button);
+        mLoginSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+    }
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid login, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptLogin() {
+        if (mAuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        mLoginView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String login = mLoginView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (TextUtils.isEmpty(password)) {
+            mPasswordView.setError(getString(R.string.error_field_required));
+            focusView = mPasswordView;
+            cancel = true;
+        } else if (!isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid login address.
+        if (TextUtils.isEmpty(login)) {
+            mLoginView.setError(getString(R.string.error_field_required));
+            focusView = mLoginView;
+            cancel = true;
+        } else if (!isLoginValid(Integer.parseInt(login))) {
+            mLoginView.setError(getString(R.string.error_invalid_login));
+            focusView = mLoginView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            showProgress(true);
+
+            UserLoginPostgresql mAuthTask = new UserLoginPostgresql(Integer.parseInt(login),
+                    password);
+            mAuthTask.execute();
+        }
+    }
+
+    private boolean isLoginValid(Integer login) {
+        int x = String.valueOf(login).length();
+        Log.d("number", String.valueOf(x));
+        return true;
+    }
+
+    private boolean isPasswordValid(String password) {
+        return password.length() > 4;
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    public class UserLoginPostgresql extends AsyncTask<Void, Void, Boolean> {
+
+        private final int mLogin;
+        private final String mPassword;
+
+        UserLoginPostgresql(int login, String password) {
+            mLogin = login;
+            mPassword = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            try {
+                Class.forName("org.postgresql.Driver");
+                String url = "jdbc:postgresql://192.168.0.14:5432/bus_can";
+                String user = "postgres";
+                String password = "bus_can";
+                Log.d("LOG_TAG", "Connexion ...  !");
+                Connection conn = DriverManager.getConnection(url, user, password);
+                Log.d("LOG_TAG", "... effective !");
+                Statement st = conn.createStatement();
+                ResultSet rs = st.executeQuery("Select * from \"Users\"");
+
+                //TODO: Verifier que les donnees saisies sont compatible avec ceux de la bdd
+                while(rs.next()) {
+                    Log.d("LOG_TAG", rs.getString(1) + " " + mLogin);
+                    Log.d("LOG_TAG", rs.getString(2) + " " + mPassword);
+                }
+
+                rs.close();
+                st.close();
+                conn.close();
+            } catch(Exception e) {
+                Log.d("LOG_TAG", "Failure " + e.getMessage() );
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            if (success) {
+                finish();
+
+                Intent intent = new Intent(LoginActivity.this, Maps.class);
+                startActivity(intent);
+            } else {
+                showProgress(false);
+
+                mPasswordView.setError(getString(R.string.error_incorrect_password));
+                mPasswordView.requestFocus();
+            }
+        }
+    }
+}
